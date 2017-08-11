60284c2b51e5944198feec2f425d5e5e2c3ee362
indhira10 <30368537+indhira10@users.noreply.github.com>
2017-08-11 17:16:16 +0530

    Create think.c

---
+++ think.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	double number;
+	printf("enter the number :");
+	scanf("%1f,&number");
+	if(number<=0.0)
+	{
+	    if(number==0.0)
+	    printf("you entered 0.");
+	    else
+	    printf("enter a positive number.");
+	    else
+	    printf("enter a negative number.");
+	}
+	return 0;
+}
